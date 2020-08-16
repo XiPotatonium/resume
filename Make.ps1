67b30a865362b51643fc9de96184bc67d7a787ed
XI <xipotatonium@outlook.com>
2020-08-16 19:57:11 +0800

    PAT分数

--- Make.ps1
+++ Make.ps1
@@ -1,2 +1,29 @@
-xelatex resume.tex
-xelatex resume-cn.tex
+function Build-Pdf {
+    param ($src)
+
+    if (!(Test-Path $src)) {
+        Write-Output "File $src not exists"
+        return
+    }
+
+    $srcFile = Get-Item $src
+    $targetName = $srcFile.BaseName + ".pdf"
+
+    if (!(Test-Path $targetName)) {
+        xelatex.exe $src
+    } else {
+        $targetFile = Get-Item $targetName
+        if ($targetFile.LastWriteTime -lt $srcFile.LastWriteTime) {
+            xelatex.exe $src
+        } else {
+            Write-Output "File $targetName is already up-to-date"
+        }
+    }
+}
+
+Build-Pdf resume.tex
+Build-Pdf resume-cn.tex
+
+Remove-Item *.aux
+Remove-Item *.log
+Remove-Item *.out
